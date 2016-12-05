8d5c0d70bef4a8cfed0f38a8c7b05e2d9d00cd7b
Toby Henderson <hendersont@gmail.com>
2016-12-05 22:03:23 +0000

    basic skeleton

---
+++ src/dotnet-adr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils; 
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new CommandLineApplication
+
+            {
+                Name = "dotnet adr",
+                FullName = "Architecture Decision Record",
+                Description = "Architecture Decision Record commandline tool",
+            };
+            
+            app.HelpOption("-h|--help");
+        }
+    }
+}
